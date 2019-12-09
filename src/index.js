7a1c085450c00c7c6f328f6a09baafd0e175ebf6
Matt Jankowski <35581471+mjanko5@users.noreply.github.com>
2019-12-09 00:39:04 -0600

    lifting state up (to board class) & creating a function

--- src/index.js
+++ src/index.js
@@ -2,28 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+//class Square extends React.Component {
+//  render() {
+//    return (
+//      <button 
+//        className="square" 
+//        onClick={() => this.props.onClick()}>
+//        {this.props.value}
+//      </button>
+//    );
+//  }
+//}
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+
+
+class Board extends React.Component {
+    
   constructor(props) {
       super(props);
       this.state = {
-          value: null,
-      };
-  }
+          squares: Array(9).fill(null),
+      }
+  }   
     
+  handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+  }    
     
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.setState({value: 'ex'})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+        />
+    );
   }
     
     
