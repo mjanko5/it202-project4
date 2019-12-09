6fa448b3509509bbc0b74048525f072fffeed420
Matt Jankowski <35581471+mjanko5@users.noreply.github.com>
2019-12-09 02:36:04 -0600

    Updates

---
+++ precache-manifest.e38ebb1252bb9b53edaf8db3204615ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb341fe976d9f90d6551ffb0bf0f986a",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "823c21d14156083c20fd",
+    "url": "/myapp/static/css/main.5e841ca0.chunk.css"
+  },
+  {
+    "revision": "6386f45b44b344ef2049",
+    "url": "/myapp/static/js/2.acbec3e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.acbec3e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "823c21d14156083c20fd",
+    "url": "/myapp/static/js/main.88d58a70.chunk.js"
+  },
+  {
+    "revision": "085eaedc6753e841dd23",
+    "url": "/myapp/static/js/runtime-main.2b4fa720.js"
+  }
+]);
